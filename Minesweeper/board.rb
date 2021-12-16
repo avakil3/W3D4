49d04d9ac5a9f83db8b190ff1c26ae2b38e63a82
avakil3 <aagamvakil@hotmail.com>
2021-12-16 17:30:16 -0500

    added Aagam's changes

--- Minesweeper/board.rb
+++ Minesweeper/board.rb
@@ -24,14 +24,51 @@ def populate
         position = [rand_row,rand_col]
         @grid[position] = "B"
     end
-end
+    
+    potential_bombs = [[row-1,col-1], [row-1,col], [row-1,col+1], [row,col-1], [row, col+1], [row+1,col-1], [row+1, col], [row+1,col+1] ]
+    
+    (0...@grid.length).each do |row|
+        (0...@grid.length).each do |col|
+            counter = 0
+            if @grid[row][col] != "B"
+                potential_bombs.each do |pos|
+                   counter +=1 if @grid[pos] == "B"
+                end
+                @grid[pos] = "_" if counter == 0  
+            end
+        end
+    end
 
+
+end
+#["B", fringe(Integer), interior(_)]
 def hidden_grid
-    @grid
+    @grid.map do |row|
+        row.map do |ele|
+            if ele == "B"
+                "*"
+            else
+                ele
+            end
+        end
+    end
 end
 
 def num_bomb
     @grid.flatten.count{|ele| ele == "B"}
 end
+
+def fringe_squares
+    @grid.flatten.count{|ele| ele.is_a?(Integer)}
+end
+
+def interior_squares
+    @grid.flatten.count{|ele| ele == "_"}
+end
+
+def remaining_blanks
+    @size - num_bomb - fringe_squares - interior_squares
+end
+
 
 end
